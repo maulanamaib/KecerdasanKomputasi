4b0ab4151df2985f8840d3d1f3b655647edf459a
maulanamaib <83963442+maulanamaib@users.noreply.github.com>
2023-06-08 20:24:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -97,8 +97,8 @@ with DecisionTree:
 
     st.write("Akurasi Model: ", accuracy2)
     
-    graf =  tree.plot_tree(clf)
-    st.graphviz_chart(graf)
+#     tree.plot_tree(clf)
+    st.graphviz_chart(tree.plot_tree(clf))
     plt.figure(figsize=(8, 6))
     plt.bar(['Akurasi'], [accuracy2])
     plt.ylim([0, 1])
