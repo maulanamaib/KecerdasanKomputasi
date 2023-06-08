d9b6e9e857d6032c6d1cc7b7c6ff78f8b14d644b
maulanamaib <83963442+maulanamaib@users.noreply.github.com>
2023-06-08 20:21:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -90,14 +90,14 @@ with DecisionTree:
 
     st.subheader("DecisionTree")
     model2 = tree.DecisionTreeClassifier()
-    model2.fit(training1, training_label1)
+    clf = model2.fit(training1, training_label1)
     
     y_pred2 = model2.predict(test1)
     accuracy2 = accuracy_score(test_label1, y_pred2)
 
     st.write("Akurasi Model: ", accuracy2)
     
-    tree.plot_tree(accuracy2)
+    tree.plot_tree(clf)
 
     plt.figure(figsize=(8, 6))
     plt.bar(['Akurasi'], [accuracy2])
